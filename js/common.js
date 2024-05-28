5d7522449630c3f889ca2a81a291d77deb1551bc
LindaMeyerhans <167539356+LindaMeyerhans@users.noreply.github.com>
2024-05-28 23:02:32 +0200

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,12 @@
+/* JS Code, der nur auf mehreren Pages verwendet wird */
+
+/* Navigation */
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
