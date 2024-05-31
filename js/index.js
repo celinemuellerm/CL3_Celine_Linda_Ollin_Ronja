2dcbc739e9cb8dc27bcf0578705542084bd0b324
celinemuellerm <167540336+celinemuellerm@users.noreply.github.com>
2024-05-31 14:27:57 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 // (1) Variablen initialisieren
 const myForm = document.getElementById("myForm");
 
-submitButton.addEventListener("click", async (event) => {
-  event.preventDefault();
-  onClickSubmit();
-});
-
+// Funktion definieren
 const onClickSubmit = async () => {
   // Speichert die Daten in der Datenbank
   await databaseClient.insertInto("formular", {
     email: "test",
   });
 };
+
+// Event-Listener hinzufügen
+myForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  await onClickSubmit();
+});
